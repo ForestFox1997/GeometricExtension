b68fa16464b7c772adb68227488250c92bc388d1
SIB\kostylev_as <artem.kostylev@evraz.com>
2024-01-24 01:41:00 +0700

    Реализованы тесты

---
+++ Tests/CalculationServiceTest.cs
@@ -0,0 +1,73 @@
+using System.Reflection;
+using static System.Math;
+using GeometricExtension.Services;
+
+namespace Tests
+{
+    /// <summary>
+    /// Тестирует функциональность <see cref="CalculationService"/>
+    /// </summary>
+    [TestClass]
+    public class CalculationServiceTest
+    {
+        /// <summary>
+        /// Проверяет <see cref="CalculationService.CalculateFigureArea"/>, принимающий произвольное кол-во
+        /// аргументов на выброс исключения, если кол-во переданных аргументов оказалось некорректно
+        /// </summary>
+        [TestMethod]
+        public void CheckInterruptWhenIncorrectArgumentsNumber()
+        {
+            CalculationService service = new();
+
+            // Ожидается выбрасывание исключения, если не было передано 1, или 3 аргумента
+            Assert.ThrowsException<ArgumentException>(() => service.CalculateFigureArea());
+            Assert.ThrowsException<ArgumentException>(() => service.CalculateFigureArea(5, 5));
+            Assert.ThrowsException<ArgumentException>(() => service.CalculateFigureArea(5, 5, 5, 5));
+            Assert.ThrowsException<ArgumentException>(() => service.CalculateFigureArea(5, 5, 5, 5, 5));
+        }
+
+        /// <summary>
+        /// Проверяет корректность метода, выполняющего вычисление площади круга по радиусу
+        /// </summary>
+        [TestMethod]
+        public void CheckCorrectnessOfCircleAreaCalculation()
+        {
+            CalculationService service = new();
+
+            double originalValue = new Random().NextDouble();
+
+            // Выполнение private метода у service
+            MethodInfo? method = service.GetType().GetTypeInfo().GetMethod(
+                "CalculateAreaOfCircle", BindingFlags.NonPublic | BindingFlags.Instance);
+            var calculatedResult = (double)method!.Invoke(service, new object[] { originalValue })!;
+
+            double inverseFormulaCalculationResult = Math.Sqrt(calculatedResult / Math.PI);
+
+            Assert.AreEqual(originalValue, inverseFormulaCalculationResult);
+        }
+
+        /// <summary>
+        /// Проверяет корректность метода, выполняющего вычисление площади треугольника по длине сторон
+        /// </summary>
+        [TestMethod]
+        public void CheckCorrectnessOfTriangleAreaCalculation()
+        {
+            CalculationService service = new();
+
+            Random random = new();
+            double firstSide = random.NextDouble(), secondSide = random.NextDouble(), thirdSide = random.NextDouble();
+
+            // Выполнение private метода у service
+            MethodInfo? method = service.GetType().GetTypeInfo().GetMethod(
+                "CalculateAreaOfTriangle", BindingFlags.NonPublic | BindingFlags.Instance);
+            var calculatedResult = (double)method!.Invoke(service,
+                new object[] { firstSide, secondSide, thirdSide })!;
+
+            // В качестве альтернативного вычисления используется альтернативная Формула Герона
+            var alternateCalculatedResult = 0.25 * Sqrt(Pow(Pow(firstSide, 2) + Pow(secondSide, 2) + Pow(thirdSide, 2), 2)
+                - (2 * (Pow(firstSide, 4) + Pow(secondSide, 4) + Pow(thirdSide, 4))));
+
+            Assert.AreEqual(calculatedResult, alternateCalculatedResult);
+        }
+    }
+}
